7be6cbb46c74cf7e03d8cde7f8d2e6aed22b6bb2
0Nom4D <arthur.adam@epitech.eu>
2021-11-09 14:42:34 +0100

    304pacman - Removing isInWall useless methods

--- sources/EngineObjects/EngineObject.py
+++ sources/EngineObjects/EngineObject.py
@@ -6,21 +6,16 @@
 ##
 
 class EngineObj:
-    def __init__(self, mapContent: list, wallChar: str, checkedChar: str) -> None:
-        self.positionX, self.positionY = self.getPositionFromMap(mapContent, wallChar, checkedChar)
+    def __init__(self, mapContent: list, checkedChar: str) -> None:
+        self.positionX, self.positionY = self.getPositionFromMap(mapContent, checkedChar)
         pass
 
-    def getPositionFromMap(self, fileContent: list, wallChar: str, checkedChar: str):
+    def getPositionFromMap(self, fileContent: list, checkedChar: str):
         for absc in range(len(fileContent)):
             for ordn in range(len(fileContent[absc])):
                 if fileContent[absc][ordn] == checkedChar:
                     return absc, ordn
         return -1, -1
-
-    def isInWalls(self, content: list, x: int, y: int, wallChar: str) -> bool:
-        if content[x + 1][y] == wallChar and content[x - 1][y] == wallChar and content[x][y + 1] == wallChar and content[x][y - 1] == wallChar:
-            return True
-        return False
 
     def getPosition(self) -> int:
         return (self.positionX, self.positionY)
